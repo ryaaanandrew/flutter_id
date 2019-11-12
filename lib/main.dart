d3bc4fb9012292f987c681936cf4024233a30062
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-11-11 22:50:37 -0500

    widget practice

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,83 @@ class Card extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
+        centerTitle: true,
         title: Text('ID Card'),
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/thumb.jpeg'),
+                radius: 70.0,
+              ),
+            ),
+            Divider(
+              height: 90.0,
+              color: Colors.grey[800],
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Text(
+              'Ryan',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Text(
+              'CURRENT LEVEL',
+              style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Text(
+              'Eight',
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'Ryan.Sibat@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
